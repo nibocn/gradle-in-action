66a19b7e4f477db7b3c42d6ab66f1256932f6894
NiBo <nibocn@gmail.com>
2017-08-23 16:48:08 +0800

    refactor: Rename InMemoryToDoRespositoryTest file name

--- chapter07/listing_07_01-02-junit-test-successful/repository/src/test/java/me/nibo/todo/repository/InMemoryToDoRepositoryTest.java
+++ chapter07/listing_07_01-02-junit-test-successful/repository/src/test/java/me/nibo/todo/repository/InMemoryToDoRepositoryTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class InMemoryToDoRespositoryTest {
+public class InMemoryToDoRepositoryTest {
     private ToDoRepository inMemoryToDoRepository;
 
     @Before
